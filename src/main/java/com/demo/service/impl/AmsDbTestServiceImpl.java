a2e560cb1bad520a13fbcc605548d7bd71c79df8
mortaceli <mortaceli@gmail.com>
2016-08-29 09:58:19 +0800

    multy

--- src/main/java/com/demo/service/impl/AmsDbTestServiceImpl.java
+++ src/main/java/com/demo/service/impl/AmsDbTestServiceImpl.java
@@ -4,6 +4,7 @@ import com.demo.dao.Token;
 import com.demo.mapper.TokenMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Created by 2015-218-pc on 8/23/16.
@@ -15,13 +16,15 @@ public class AmsDbTestServiceImpl {
     @Autowired
     private TokenMapper tokenMapper;
 
+    @Transactional()
     public void getAllAms(){
 
         //TokenDAOImpl tokenDAOImpl = new TokenDAOImpl();
 
         Token token = tokenMapper.selectByPrimaryKey( Integer.valueOf(11) );
 
-        System.out.println(token.getToken());
+        //System.out.println(token.getToken());
+        System.out.println(token.toString());
 
     }
 
