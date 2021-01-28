0155230c2567ab0f91c163c9df7a96667ba942e1
mort <mortaceli@gmail.com>
2021-01-28 19:54:14 +0800

    comit

--- src/main/java/com/demo/utils/JacksonUtils.java
+++ src/main/java/com/demo/utils/JacksonUtils.java
@@ -98,6 +98,9 @@ public class JacksonUtils {
      * </bean>
      */
 
+    /**
+     * 次方式有缓存 同一个objectMapper 只会第一次执行此方法 后续会从缓存里拉取类信息 导致不能在运行时动态改变序列化方式 不推荐使用
+     */
     public static class CustomAnnotationSerializer extends JacksonAnnotationIntrospector {
 
         @Override
